c68770eacf949197cf4148cadecaa621bfdccd0a
pyavchik@gmail.com <pyavchik@gmail.com>
2019-03-05 22:03:43 +0200

    empty project for hw counters

---
+++ project-counters/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Counter extends React.Component {
+    render() {
+        return <button>Button</button>
+    }
+}
+
+ReactDOM.render(<Counter />, document.getElementById('root'));
+
