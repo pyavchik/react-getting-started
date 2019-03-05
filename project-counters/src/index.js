56775fcfbfc88a69f584dc788cd75bc1f46f9956
pyavchik@gmail.com <pyavchik@gmail.com>
2019-03-05 22:54:28 +0200

    added first steps

--- project-counters/src/index.js
+++ project-counters/src/index.js
@@ -3,9 +3,17 @@ import ReactDOM from 'react-dom';
 
 class Counter extends React.Component {
     render() {
-        return <button>Button</button>
+        return (
+            <div>
+               {this.props.title}
+               <button>{this.props.min}</button>
+               <button>{this.props.max}</button>
+               <button>reset</button>
+            </div>
+        );
     }
 }
 
-ReactDOM.render(<Counter />, document.getElementById('root'));
+ReactDOM.render(<Counter title="Counter A" min={-1} max={+1}/> , document.getElementById('root'));
+ReactDOM.render(<Counter title="Counter B" min={-1} max={+1}/> , document.getElementById('root2'));
 
