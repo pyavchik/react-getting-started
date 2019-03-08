4a4908d4022ae1529e2679f646a0bbf455bef6b3
pyavchik@gmail.com <pyavchik@gmail.com>
2019-03-08 16:50:12 +0200

    jsx added

--- project-bmi-calculator/src/index.js
+++ project-bmi-calculator/src/index.js
@@ -1,6 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const bmiCalc = (<h1>bmi Calculator</h1>);
 
-ReactDOM.render(bmiCalc, document.getElementById('root'));
+class BmiCalculator extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            value: { min: 90, max: 245 , actual: 175},
+        };
+    }
+
+
+
+    render() {
+        return (
+            <div>
+                <div>Height: 90 <input type="range"  min={this.state.value.min} max={this.state.value.max}  /> 245 <b>151 cm</b></div>
+                <div>Weight: 35 <input type="range" min={this.state.value.min} max={this.state.value.max} /> 200 <b>75 kg</b></div>
+                <span>BMI:</span>
+                <b>32.9 Obese</b>
+            </div>
+        );
+    }
+}
+
+
+ReactDOM.render(
+    <BmiCalculator />,
+    document.getElementById('root')
+);
