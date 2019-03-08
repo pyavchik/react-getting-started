c7404b88e397a083e79975ed1e05911a2070f555
pyavchik@gmail.com <pyavchik@gmail.com>
2019-03-08 14:43:52 +0200

    prepared empty project for digital clock

---
+++ project-digital-clock/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const digitalClock = (
+    <span>22:22:22</span>
+);
+
+ReactDOM.render(digitalClock, document.getElementById('root'));
+
