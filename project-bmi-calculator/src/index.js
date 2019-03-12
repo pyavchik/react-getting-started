ec194c308bb5bed9de024e1947ddcc24f4a5e2c9
pyavchik@gmail.com <pyavchik@gmail.com>
2019-03-12 21:23:05 +0200

    finished project bmi calculator

--- project-bmi-calculator/src/index.js
+++ project-bmi-calculator/src/index.js
@@ -1,71 +1,86 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
-class Slider extends React.Component {
+class App extends React.Component {
     constructor(props) {
         super(props);
-
         this.state = {
-            value: this.props.value,
+            height: 175,
+            weight: 75
         };
     }
 
-
-    handleChange = (event) => {
-        this.setState({value: event.target.value})
+    handleHeightChange = (event) => {
+        this.setState({ height: event.target.value });
+    };
+    handleWeightChange = event => {
+        this.setState({ weight: event.target.value });
     };
 
+
     render() {
+        let { height, weight } = this.state;
+        let bmi = (weight / (height ** 2) * 10000).toFixed(2);
+        let bmiResult;
+        if (bmi <= 15){
+            bmiResult = "Very severely underweight";
+        } else if ((bmi > 15) && (bmi < 16)){
+            bmiResult = "Very severely underweight";
+        } else if ((bmi > 16) && (bmi < 18.5)){
+            bmiResult = "Underweight"
+        } else if ((bmi > 18.5) && (bmi < 25)){
+            bmiResult = "Normal (healthy weight)"
+        } else if ((bmi > 18.5) && (bmi < 25)){
+            bmiResult = "Normal (healthy weight)"
+        } else if ((bmi > 25) && (bmi < 30)){
+            bmiResult = "Overweight"
+        } else if ((bmi > 30) && (bmi < 35)){
+            bmiResult = "Obese Class I (Moderately obese)"
+        } else if ((bmi > 35) && (bmi < 40)){
+            bmiResult = "Obese Class II (Severely obese)"
+        } else if ((bmi > 40) && (bmi < 45)){
+            bmiResult = "Obese Class III (Very severely obese)"
+        } else if ((bmi > 45) && (bmi < 50)){
+            bmiResult = "Obese Class IV (Morbidly Obese)"
+        } else if ((bmi > 50) && (bmi < 60)){
+            bmiResult = "Obese Class V (Super Obese)"
+        } else if (bmi > 60){
+            bmiResult = "Obese Class VI (Hyper Obese)"
+        } else {
+            bmiResult = "something went wrong!"
+        }
+
         return (
             <div>
-                <div>{this.props.title}: {this.props.min}
-                    <input type="range"
-                           min={this.props.min} max={this.props.max}
-                           value={this.state.value}
-                           onChange={this.handleChange}
-                           step="1"/>  {this.props.max}
-                    <b>{this.state.value} {this.props.units}</b>
-                </div>
+                <Slider title={"Height"} min={90} max={245} value={height} onChange={this.handleHeightChange} units="cm"/>
+                <Slider title={"Weight"} min={35} max={200} value={weight} onChange={this.handleWeightChange} units="kg"/>
+                <div>BMI: <b>{bmi} {bmiResult}</b> </div>
             </div>
         );
     }
+
 }
 
-class BMI extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            height: 100,
-            weight: 100
-        };
-    }
-
+class Slider extends React.Component {
     render() {
+
         return (
             <div>
-                <div>
-                    {this.props.title} {this.state.height} {"Underweight"}
-                </div>
+                <span>{this.props.title}: {this.props.min}</span>
+                <input type="range"
+                       title={this.props.title}
+                       min={this.props.min}
+                       max={this.props.max}
+                       value={this.props.value}
+                       onChange={this.props.onChange}
+                       step="1"/>
+                <span>{this.props.max} </span> <b>{this.props.value} {this.props.units}</b>
             </div>
         );
     }
-}
-
-
-
-function App() {
-    return (
-        <div>
-            <Slider title="Height" min={90} max={245} units="cm" value={175}/>
-            <Slider title = "Weight" min={35} max={200} units="kg" value={75}/>
-            <BMI title="BMI" />
-        </div>
-    );
 }
 
 ReactDOM.render(
-    <App />,
+    <App  />,
     document.getElementById('root')
 );
