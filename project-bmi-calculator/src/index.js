c35c3fea9eca31dce757273c0d90e2469865912f
pyavchik@gmail.com <pyavchik@gmail.com>
2019-03-13 21:26:32 +0200

    update bmi project

--- project-bmi-calculator/src/index.js
+++ project-bmi-calculator/src/index.js
@@ -11,50 +11,61 @@ class App extends React.Component {
     }
 
     handleHeightChange = (event) => {
-        this.setState({ height: event.target.value });
+        this.setState({height: event.target.value});
     };
     handleWeightChange = event => {
-        this.setState({ weight: event.target.value });
+        this.setState({weight: event.target.value});
     };
 
 
-    render() {
-        let { height, weight } = this.state;
-        let bmi = (weight / (height ** 2) * 10000).toFixed(2);
-        let bmiResult;
-        if (bmi <= 15){
+    getBmiResult(bmi) {
+        let {bmiResult} = this.state;
+        if (bmi <= 15) {
             bmiResult = "Very severely underweight";
-        } else if ((bmi > 15) && (bmi < 16)){
+        } else if ((bmi > 15) && (bmi <= 16)) {
             bmiResult = "Very severely underweight";
-        } else if ((bmi > 16) && (bmi < 18.5)){
-            bmiResult = "Underweight"
-        } else if ((bmi > 18.5) && (bmi < 25)){
-            bmiResult = "Normal (healthy weight)"
-        } else if ((bmi > 18.5) && (bmi < 25)){
-            bmiResult = "Normal (healthy weight)"
-        } else if ((bmi > 25) && (bmi < 30)){
-            bmiResult = "Overweight"
-        } else if ((bmi > 30) && (bmi < 35)){
-            bmiResult = "Obese Class I (Moderately obese)"
-        } else if ((bmi > 35) && (bmi < 40)){
-            bmiResult = "Obese Class II (Severely obese)"
-        } else if ((bmi > 40) && (bmi < 45)){
-            bmiResult = "Obese Class III (Very severely obese)"
-        } else if ((bmi > 45) && (bmi < 50)){
-            bmiResult = "Obese Class IV (Morbidly Obese)"
-        } else if ((bmi > 50) && (bmi < 60)){
-            bmiResult = "Obese Class V (Super Obese)"
-        } else if (bmi > 60){
-            bmiResult = "Obese Class VI (Hyper Obese)"
+        } else if ((bmi > 16) && (bmi <= 18.5)) {
+            bmiResult = "Underweight";
+        } else if ((bmi > 18.5) && (bmi <= 25)) {
+            bmiResult = "Normal (healthy weight)";
+        } else if ((bmi > 25) && (bmi <= 30)) {
+            bmiResult = "Overweight";
+        } else if ((bmi > 30) && (bmi <= 35)) {
+            bmiResult = "Obese Class I (Moderately obese)";
+        } else if ((bmi > 35) && (bmi <= 40)) {
+            bmiResult = "Obese Class II (Severely obese)";
+        } else if ((bmi > 40) && (bmi <= 45)) {
+            bmiResult = "Obese Class III (Very severely obese)";
+        } else if ((bmi > 45) && (bmi <= 50)) {
+            bmiResult = "Obese Class IV (Morbidly Obese)";
+        } else if ((bmi > 50) && (bmi <= 60)) {
+            bmiResult = "Obese Class V (Super Obese)";
+        } else if (bmi > 60) {
+            bmiResult = "Obese Class VI (Hyper Obese)";
         } else {
-            bmiResult = "something went wrong!"
+            bmiResult = "something went wrong!";
         }
+        return bmiResult;
+    }
+
+    getBmi(weight, height) {
+        return weight / ((height / 100) ** 2);
+    }
+
+    render() {
+        let {height, weight} = this.state;
+        let bmi = this.getBmi(weight, height);
+        let bmiResult;
+        bmiResult = this.getBmiResult(bmi);
+
 
         return (
             <div>
-                <Slider title={"Height"} min={90} max={245} value={height} onChange={this.handleHeightChange} units="cm"/>
-                <Slider title={"Weight"} min={35} max={200} value={weight} onChange={this.handleWeightChange} units="kg"/>
-                <div>BMI: <b>{bmi} {bmiResult}</b> </div>
+                <Slider title={"Height"} min={90} max={245} value={height} onChange={this.handleHeightChange}
+                        units="cm"/>
+                <Slider title={"Weight"} min={35} max={200} value={weight} onChange={this.handleWeightChange}
+                        units="kg"/>
+                <div>BMI: <b>{bmi.toFixed(1)} {bmiResult}</b></div>
             </div>
         );
     }
@@ -81,6 +92,6 @@ class Slider extends React.Component {
 }
 
 ReactDOM.render(
-    <App  />,
+    <App/>,
     document.getElementById('root')
 );
