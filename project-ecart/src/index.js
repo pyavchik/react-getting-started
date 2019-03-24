517ff69f42114871898992a93ab1c33c231c9494
pyavchik@gmail.com <pyavchik@gmail.com>
2019-03-24 21:37:25 +0200

    added inventory

--- project-ecart/src/index.js
+++ project-ecart/src/index.js
@@ -1,40 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component{
+
+class Cart extends React.Component {
     render() {
         return (
             <div>
                 <div>
-                    <h3>Cart</h3>
-                    <div>
-                        <p>
-                            <i>Your cart is empty.</i>
-                        </p>
-                    </div>
-                    <div>Total: $0
-                        <button disabled="">Checkout</button>
-                    </div>
+                    <p> Your cart is empty </p>
                 </div>
-                <hr/>
                 <div>
-                    <h3>Inventory</h3>
-                    <div>
-                        <div>Apple – $10
-                            <button>Buy</button> (0 selected, 12 in stock)
-                        </div>
-                        <div>Melon – $20
-                            <button>Buy</button> (0 selected, 5 in stock)
-                        </div>
-                        <div>Orange – $8
-                            <button>Buy</button> (0 selected, 20 in stock)
-                        </div>
-                    </div>
+                    <span>Total: $0</span>
+                    <button type="button" onClick={this.buyClick} disabled>Checkout</button>
                 </div>
             </div>
 
-        )
+        );
     }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Inventory extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selected: 0,
+            stock: this.props.stock,
+            emptyStock: false
+        };
+    }
+    buyClick = () => {
+        this.setState(({ selected, stock }) => ({
+            selected: selected + 1,
+            stock: stock - 1,
+        }));
+    };
+
+    render() {
+        return (
+            <div>
+                <span> {this.props.title} – $</span>
+                <span> {this.props.price}</span>
+                <button type="button" onClick={this.buyClick} disabled={!this.state.stock > 0}>Buy</button>
+                <span> ({this.state.selected} selected, {this.state.stock} in stock)</span>
+            </div>
+
+        );
+    }
+}
+
+function App() {
+    return (
+        <div>
+            <h3>Cart</h3>
+            <Cart title={"Apple"} price={10} selectedCount={0} inStockCount={12}/>
+            <hr/>
+            <h3>Inventory</h3>
+            <Inventory title={"Apple"} price={10} selected={0} stock={12}/>
+            <Inventory title={"Melon"} price={20} selected={0} stock={5}/>
+            <Inventory title={"Orange"} price={8} selected={0} stock={20}/>
+
+        </div>
+    );
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
